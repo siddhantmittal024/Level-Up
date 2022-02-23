3ad960dcea59274acf38f3d560075e3fe07c693d
Siddhant Mittal <sidmit2001@gmail.com>
2022-02-23 19:26:23 +0530

    image fixes

--- src/utils/cardTitles.js
+++ src/utils/cardTitles.js
@@ -1,34 +1,33 @@
 export const StudentDashboardTitles = [
   {
     title: 'Jobs',
-    imageUrl:
-      'https://cdn.searchenginejournal.com/wp-content/uploads/2017/06/shutterstock_268688447.jpg'
+    imageUrl: '/src/images/jobs.jpg'
   },
   {
     title: 'Internships',
-    imageUrl: 'https://www.leisurejobs.com/getasset/1340b424-e46b-4d29-9005-c6ec1eb4dfcb/'
+    imageUrl: '/src/images/internships.jpg'
   },
   {
     title: 'Higher Education',
-    imageUrl: 'https://ubuniwp.s3.eu-west-2.amazonaws.com/dashboard/wp-content/uploads/2019/01/27160244/technology-in-higher-education.jpg'
+    imageUrl: '/src/images/higher-education.jpg'
   },
   {
     title: 'Study Material',
-    imageUrl: 'https://www.clearias.com/up/UPSC-Studymaterials-of-ClearIAS.png'
+    imageUrl: '/src/images/study-material.png'
   },
   {
     title: 'Other Opportunities',
-    imageUrl: 'https://leverageedu.com/blog/wp-content/uploads/2019/12/Youth-Exchange-Programs.png'
+    imageUrl: '/src/images/other-opportunities.png'
   }
 ];
 
 export const SubSectiontitles = [
   {
     title: 'On Campus',
-    imageUrl: 'https://uiic.aktu.ac.in/wp-content/uploads/2019/01/21b9ad30781a3312d12f4eb3aa07aa4c_XL.jpg'
+    imageUrl: '/src/images/on-campus.jpg'
   },
   {
     title: 'Off Campus',
-    imageUrl: 'https://leverageedu.com/blog/wp-content/uploads/2019/12/Off-Campus-Placements-or-Off-Campus-Placement.jpg'
+    imageUrl: '/src/images/off-campus.jpg'
   }
 ];
