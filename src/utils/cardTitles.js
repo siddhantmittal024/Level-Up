6dc935d7838008db8dfa43b72bc94bc631984260
Siddhant Mittal <sidmit2001@gmail.com>
2022-04-13 12:53:26 +0530

    fixes

--- src/utils/cardTitles.js
+++ src/utils/cardTitles.js
@@ -1,67 +1,67 @@
 export const StudentDashboardTitles = [
   {
     title: 'Jobs',
-    imageUrl: '/src/images/jobs.jpg'
+    imageUrl: '/images/jobs.jpg'
   },
   {
     title: 'Internships',
-    imageUrl: '/src/images/internships.jpg'
+    imageUrl: '/images/internships.jpg'
   },
   {
     title: 'Higher Education',
-    imageUrl: '/src/images/higher-education.jpg'
+    imageUrl: '/images/higher-education.jpg'
   },
   {
     title: 'Study Material',
-    imageUrl: '/src/images/study-material.png'
+    imageUrl: '/images/study-material.png'
   },
   {
     title: 'Other Opportunities',
-    imageUrl: '/src/images/other-opportunities.png'
+    imageUrl: '/images/other-opportunities.png'
   }
 ];
 
 export const CoordinatorDashboardTites = [
   {
     title: 'Jobs',
-    imageUrl: '/src/images/jobs.jpg'
+    imageUrl: '/images/jobs.jpg'
   },
   {
     title: 'Internships',
-    imageUrl: '/src/images/internships.jpg'
+    imageUrl: '/images/internships.jpg'
   },
   {
     title: 'Other Opportunities',
-    imageUrl: '/src/images/other-opportunities.png'
+    imageUrl: '/images/other-opportunities.png'
   }
 ];
 
 export const AdminDashboardTitles = [
   {
     title: 'Jobs',
-    imageUrl: '/src/images/jobs.jpg'
+    imageUrl: '/images/jobs.jpg'
   },
   {
     title: 'Internships',
-    imageUrl: '/src/images/internships.jpg'
+    imageUrl: '/images/internships.jpg'
   },
   {
     title: 'Other Opportunities',
-    imageUrl: '/src/images/other-opportunities.png'
+    imageUrl: '/images/other-opportunities.png'
   },
   {
     title: 'Users',
-    imageUrl: '/src/images/user.jpg'
+    imageUrl: '/images/user.jpg'
   }
 ];
 
 export const SubSectiontitles = [
   {
     title: 'On Campus',
-    imageUrl: '/src/images/on-campus.jpg'
+    imageUrl: '/images/on-campus.jpg'
   },
   {
     title: 'Off Campus',
-    imageUrl: '/src/images/off-campus.jpg'
+    imageUrl: '/images/off-campus.jpg'
   }
 ];
