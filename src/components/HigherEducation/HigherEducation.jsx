afa7ad33b4b4d36af841ee6fd5ec6f2e0bd07e0d
Siddhant Mittal <sidmit2001@gmail.com>
2022-04-24 03:35:20 +0530

    Static Pages (Higher Education Page)!

--- src/components/HigherEducation/HigherEducation.jsx
+++ src/components/HigherEducation/HigherEducation.jsx
@@ -3,18 +3,27 @@ import ListItem from '@mui/material/ListItem';
 import { Button, Container, Paper, Box, Typography } from '@mui/material';
 const HigherEducation = () => {
   return (
-    <div>
-      <Typography variant="h3" align="center" component="div" gutterBottom>
-        Higher Education
-      </Typography>
-      <Box m={2}>
+    <Container>
+      <Box m={2} sx={{ marginTop: '25px' }}>
         <Box sx={{ backgroundColor: '#75A2C9' }}>
-          <Typography variant="h6" component="div" gutterBottom align="center">
-            Core Subjects(CSE):
+          <Typography
+            variant="h4"
+            component="div"
+            gutterBottom
+            align="center"
+            sx={{ padding: '15px', color: 'white', textTransform: 'uppercase' }}
+          >
+            Core Subjects(CSE) for GATE:
           </Typography>
         </Box>
-
-        <Typography variant="body2" gutterBottom>
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{
+            fontSize: '18px',
+            marginBottom: '15px'
+          }}
+        >
           <ListItem sx={{ display: 'list-item' }}>
             C Programming And Data Structure
           </ListItem>
@@ -31,7 +40,9 @@ const HigherEducation = () => {
           <ListItem sx={{ display: 'list-item' }}>
             Computer Architecture/ Organization
           </ListItem>
-          <p>(Not Included In Gate)</p>
+          <p style={{ fontWeight: 'bold', fontSize: '20px' }}>
+            (Not Included In Gate)
+          </p>
           <ListItem sx={{ display: 'list-item' }}>Java</ListItem>
           <ListItem sx={{ display: 'list-item' }}>
             Artificial Intelligence
@@ -48,15 +59,36 @@ const HigherEducation = () => {
           </ListItem>
         </Typography>
 
-        <Box sx={{ backgroundColor: '#75A2C9' }}>
-          <Typography variant="h6" component="div" align="center" gutterBottom>
+        <Box sx={{ backgroundColor: '#75A2C9', marginBottom: '15px' }}>
+          <Typography
+            variant="h5"
+            component="div"
+            align="center"
+            gutterBottom
+            sx={{ padding: '15px', color: 'white', textTransform: 'uppercase' }}
+          >
             Theory of Automata
           </Typography>
         </Box>
-        <Typography variant="body1" component="div" gutterBottom>
+        <Typography
+          variant="body1"
+          component="div"
+          gutterBottom
+          sx={{
+            fontSize: '20px',
+            marginBottom: '15px'
+          }}
+        >
           Video Lectures:
         </Typography>
-        <Typography variant="body2" gutterBottom>
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{
+            fontSize: '18px',
+            marginBottom: '15px'
+          }}
+        >
           <ListItem sx={{ display: 'list-item' }}>
             MIT OpenSource
             (https://www.youtube.com/channel/UCEBb1b_L6zDS3xTUrIALZOw)
@@ -71,20 +103,41 @@ const HigherEducation = () => {
           <ListItem sx={{ display: 'list-item' }}>
             Unacademy Computer Science
           </ListItem>
-          <p>Other Resources:</p>
+          <p style={{ fontSize: '20px' }}>Other Resources:</p>
           <ListItem sx={{ display: 'list-item' }}>Geeks for Geeks</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Lecture Notes</ListItem>
         </Typography>
 
-        <Box sx={{ backgroundColor: '#75A2C9' }}>
-          <Typography variant="h6" component="div" align="center" gutterBottom>
+        <Box sx={{ backgroundColor: '#75A2C9', marginBottom: '15px' }}>
+          <Typography
+            variant="h5"
+            component="div"
+            align="center"
+            gutterBottom
+            sx={{ padding: '15px', color: 'white', textTransform: 'uppercase' }}
+          >
             Operating System
           </Typography>
         </Box>
-        <Typography variant="body1" component="div" gutterBottom>
+        <Typography
+          variant="body1"
+          component="div"
+          gutterBottom
+          sx={{
+            fontSize: '20px',
+            marginBottom: '15px'
+          }}
+        >
           Video Lectures:
         </Typography>
-        <Typography variant="body2" gutterBottom>
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{
+            fontSize: '18px',
+            marginBottom: '15px'
+          }}
+        >
           <ListItem sx={{ display: 'list-item' }}>MIT OpenSource</ListItem>
           <ListItem sx={{ display: 'list-item' }}>
             Gate Lectures By Ravindra Babu Ravula
@@ -94,81 +147,165 @@ const HigherEducation = () => {
           </ListItem>
           <ListItem sx={{ display: 'list-item' }}>Knowledge GATE</ListItem>
           <ListItem sx={{ display: 'list-item' }}>NPTEL</ListItem>
-          <p>Other Resources:</p>
+          <p style={{ fontSize: '20px' }}>Other Resources:</p>
           <ListItem sx={{ display: 'list-item' }}>Geeks for Geeks</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Lecture Notes</ListItem>
         </Typography>
 
-        <Box sx={{ backgroundColor: '#75A2C9' }}>
-          <Typography variant="h6" component="div" align="center" gutterBottom>
+        <Box sx={{ backgroundColor: '#75A2C9', marginBottom: '15px' }}>
+          <Typography
+            variant="h5"
+            component="div"
+            align="center"
+            gutterBottom
+            sx={{ padding: '15px', color: 'white', textTransform: 'uppercase' }}
+          >
             C Programming
           </Typography>
         </Box>
-        <Typography variant="body1" component="div" gutterBottom>
+        <Typography
+          variant="body1"
+          component="div"
+          gutterBottom
+          sx={{
+            fontSize: '20px',
+            marginBottom: '15px'
+          }}
+        >
           Video Lectures:
         </Typography>
-        <Typography variant="body2" gutterBottom>
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{
+            fontSize: '18px',
+            marginBottom: '15px'
+          }}
+        >
           <ListItem sx={{ display: 'list-item' }}>MIT OpenSource</ListItem>
           <ListItem sx={{ display: 'list-item' }}>
             Programming in C by Saurabh Shukla
           </ListItem>
           <ListItem sx={{ display: 'list-item' }}>NPTEL</ListItem>
-          <p>Other Resources:</p>
+          <p style={{ fontSize: '20px' }}>Other Resources:</p>
           <ListItem sx={{ display: 'list-item' }}>Geeks for Geeks</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Lecture Notes</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Tutorial Point</ListItem>
         </Typography>
 
-        <Box sx={{ backgroundColor: '#75A2C9' }}>
-          <Typography variant="h6" component="div" align="center" gutterBottom>
+        <Box sx={{ backgroundColor: '#75A2C9', marginBottom: '15px' }}>
+          <Typography
+            variant="h5"
+            component="div"
+            align="center"
+            gutterBottom
+            sx={{ padding: '15px', color: 'white', textTransform: 'uppercase' }}
+          >
             Data Structures
           </Typography>
         </Box>
-        <Typography variant="body1" component="div" gutterBottom>
+        <Typography
+          variant="body1"
+          component="div"
+          gutterBottom
+          sx={{
+            fontSize: '20px',
+            marginBottom: '15px'
+          }}
+        >
           Video Lectures:
         </Typography>
-        <Typography variant="body2" gutterBottom>
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{
+            fontSize: '18px',
+            marginBottom: '15px'
+          }}
+        >
           <ListItem sx={{ display: 'list-item' }}>MIT OpenSource</ListItem>
           <ListItem sx={{ display: 'list-item' }}>
             Data Structure by Saurabh Shukla
           </ListItem>
           <ListItem sx={{ display: 'list-item' }}>NPTEL</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Knowledge GATE</ListItem>
-          <p>Other Resources:</p>
+          <p style={{ fontSize: '20px' }}>Other Resources:</p>
           <ListItem sx={{ display: 'list-item' }}>Geeks for Geeks</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Lecture Notes</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Tutorial Point</ListItem>
         </Typography>
 
-        <Box sx={{ backgroundColor: '#75A2C9' }}>
-          <Typography variant="h6" component="div" align="center" gutterBottom>
+        <Box sx={{ backgroundColor: '#75A2C9', marginBottom: '15px' }}>
+          <Typography
+            variant="h5"
+            component="div"
+            align="center"
+            gutterBottom
+            sx={{ padding: '15px', color: 'white', textTransform: 'uppercase' }}
+          >
             Digital Design
           </Typography>
         </Box>
-        <Typography variant="body1" component="div" gutterBottom>
+        <Typography
+          variant="body1"
+          component="div"
+          gutterBottom
+          sx={{
+            fontSize: '20px',
+            marginBottom: '15px'
+          }}
+        >
           Video Lectures:
         </Typography>
-        <Typography variant="body2" gutterBottom>
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{
+            fontSize: '18px',
+            marginBottom: '15px'
+          }}
+        >
           <ListItem sx={{ display: 'list-item' }}>Neso Academy</ListItem>
           <ListItem sx={{ display: 'list-item' }}>
             Easy Engineering Classes
           </ListItem>
           <ListItem sx={{ display: 'list-item' }}>NPTEL</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Knowledge GATE</ListItem>
-          <p>Other Resources:</p>
+          <p style={{ fontSize: '20px' }}>Other Resources:</p>
           <ListItem sx={{ display: 'list-item' }}>Geeks for Geeks</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Lecture Notes</ListItem>
         </Typography>
 
-        <Box sx={{ backgroundColor: '#75A2C9' }}>
-          <Typography variant="h6" component="div" align="center" gutterBottom>
+        <Box sx={{ backgroundColor: '#75A2C9', marginBottom: '15px' }}>
+          <Typography
+            variant="h5"
+            component="div"
+            align="center"
+            gutterBottom
+            sx={{ padding: '15px', color: 'white', textTransform: 'uppercase' }}
+          >
             DBMS
           </Typography>
         </Box>
-        <Typography variant="body1" component="div" gutterBottom>
+        <Typography
+          variant="body1"
+          component="div"
+          gutterBottom
+          sx={{
+            fontSize: '20px',
+            marginBottom: '15px'
+          }}
+        >
           Video Lectures:
         </Typography>
-        <Typography variant="body2" gutterBottom>
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{
+            fontSize: '18px',
+            marginBottom: '15px'
+          }}
+        >
           <ListItem sx={{ display: 'list-item' }}>
             Gate Lectures By Ravindra Babu Ravula
           </ListItem>
@@ -177,20 +314,41 @@ const HigherEducation = () => {
           </ListItem>
           <ListItem sx={{ display: 'list-item' }}>NPTEL</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Knowledge GATE</ListItem>
-          <p>Other Resources:</p>
+          <p style={{ fontSize: '20px' }}>Other Resources:</p>
           <ListItem sx={{ display: 'list-item' }}>Geeks for Geeks</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Lecture Notes</ListItem>
         </Typography>
 
-        <Box sx={{ backgroundColor: '#75A2C9' }}>
-          <Typography variant="h6" component="div" align="center" gutterBottom>
+        <Box sx={{ backgroundColor: '#75A2C9', marginBottom: '15px' }}>
+          <Typography
+            variant="h5"
+            component="div"
+            align="center"
+            gutterBottom
+            sx={{ padding: '15px', color: 'white', textTransform: 'uppercase' }}
+          >
             Compiler Design
           </Typography>
         </Box>
-        <Typography variant="body1" component="div" gutterBottom>
+        <Typography
+          variant="body1"
+          component="div"
+          gutterBottom
+          sx={{
+            fontSize: '20px',
+            marginBottom: '15px'
+          }}
+        >
           Video Lectures:
         </Typography>
-        <Typography variant="body2" gutterBottom>
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{
+            fontSize: '18px',
+            marginBottom: '15px'
+          }}
+        >
           <ListItem sx={{ display: 'list-item' }}>
             Gate Lectures By Ravindra Babu Ravula
           </ListItem>
@@ -199,40 +357,82 @@ const HigherEducation = () => {
           </ListItem>
           <ListItem sx={{ display: 'list-item' }}>NPTEL</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Knowledge GATE</ListItem>
-          <p>Other Resources:</p>
+          <p style={{ fontSize: '20px' }}>Other Resources:</p>
           <ListItem sx={{ display: 'list-item' }}>Geeks for Geeks</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Lecture Notes</ListItem>
         </Typography>
 
-        <Box sx={{ backgroundColor: '#75A2C9' }}>
-          <Typography variant="h6" component="div" align="center" gutterBottom>
+        <Box sx={{ backgroundColor: '#75A2C9', marginBottom: '15px' }}>
+          <Typography
+            variant="h5"
+            component="div"
+            align="center"
+            gutterBottom
+            sx={{ padding: '15px', color: 'white', textTransform: 'uppercase' }}
+          >
             Computer Architecture
           </Typography>
         </Box>
-        <Typography variant="body1" component="div" gutterBottom>
+        <Typography
+          variant="body1"
+          component="div"
+          gutterBottom
+          sx={{
+            fontSize: '20px',
+            marginBottom: '15px'
+          }}
+        >
           Video Lectures:
         </Typography>
-        <Typography variant="body2" gutterBottom>
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{
+            fontSize: '18px',
+            marginBottom: '15px'
+          }}
+        >
           <ListItem sx={{ display: 'list-item' }}>Well Academy</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Gate Book</ListItem>
           <ListItem sx={{ display: 'list-item' }}>NPTEL</ListItem>
           <ListItem sx={{ display: 'list-item' }}>
             Easy Engineering Classes
           </ListItem>
-          <p>Other Resources:</p>
+          <p style={{ fontSize: '20px' }}>Other Resources:</p>
           <ListItem sx={{ display: 'list-item' }}>Geeks for Geeks</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Lecture Notes</ListItem>
         </Typography>
 
-        <Box sx={{ backgroundColor: '#75A2C9' }}>
-          <Typography variant="h6" component="div" align="center" gutterBottom>
+        <Box sx={{ backgroundColor: '#75A2C9', marginBottom: '15px' }}>
+          <Typography
+            variant="h5"
+            component="div"
+            align="center"
+            gutterBottom
+            sx={{ padding: '15px', color: 'white', textTransform: 'uppercase' }}
+          >
             Computer Networks
           </Typography>
         </Box>
-        <Typography variant="body1" component="div" gutterBottom>
+        <Typography
+          variant="body1"
+          component="div"
+          gutterBottom
+          sx={{
+            fontSize: '20px',
+            marginBottom: '15px'
+          }}
+        >
           Video Lectures:
         </Typography>
-        <Typography variant="body2" gutterBottom>
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{
+            fontSize: '18px',
+            marginBottom: '15px'
+          }}
+        >
           <ListItem sx={{ display: 'list-item' }}>Well Academy</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Knowledge GATE</ListItem>
           <ListItem sx={{ display: 'list-item' }}>NPTEL</ListItem>
@@ -242,20 +442,41 @@ const HigherEducation = () => {
           <ListItem sx={{ display: 'list-item' }}>
             Gate Lecture By Ravindra Babu Ravula
           </ListItem>
-          <p>Other Resources:</p>
+          <p style={{ fontSize: '20px' }}>Other Resources:</p>
           <ListItem sx={{ display: 'list-item' }}>Geeks for Geeks</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Lecture Notes</ListItem>
         </Typography>
 
-        <Box sx={{ backgroundColor: '#75A2C9' }}>
-          <Typography variant="h6" component="div" align="center" gutterBottom>
-            Algorithm
+        <Box sx={{ backgroundColor: '#75A2C9', marginBottom: '15px' }}>
+          <Typography
+            variant="h5"
+            component="div"
+            align="center"
+            gutterBottom
+            sx={{ padding: '15px', color: 'white', textTransform: 'uppercase' }}
+          >
+            Algorithms
           </Typography>
         </Box>
-        <Typography variant="body1" component="div" gutterBottom>
+        <Typography
+          variant="body1"
+          component="div"
+          gutterBottom
+          sx={{
+            fontSize: '20px',
+            marginBottom: '15px'
+          }}
+        >
           Video Lectures:
         </Typography>
-        <Typography variant="body2" gutterBottom>
+        <Typography
+          variant="body2"
+          gutterBottom
+          sx={{
+            fontSize: '18px',
+            marginBottom: '15px'
+          }}
+        >
           <ListItem sx={{ display: 'list-item' }}>Well Academy</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Knowledge GATE</ListItem>
           <ListItem sx={{ display: 'list-item' }}>NPTEL</ListItem>
@@ -265,12 +486,12 @@ const HigherEducation = () => {
           <ListItem sx={{ display: 'list-item' }}>
             Gate Lecture By Ravindra Babu Ravula
           </ListItem>
-          <p>Other Resources:</p>
+          <p style={{ fontSize: '20px' }}>Other Resources:</p>
           <ListItem sx={{ display: 'list-item' }}>Geeks for Geeks</ListItem>
           <ListItem sx={{ display: 'list-item' }}>Lecture Notes</ListItem>
         </Typography>
       </Box>
-    </div>
+    </Container>
   );
 };
 
